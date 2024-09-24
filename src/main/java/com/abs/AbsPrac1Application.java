9d57349b5b854a2002f4ceb9aa7135aa408931d2
Amzad-506 <amzadbasha5066@gmail.com>
2024-09-24 13:05:17 +0530

    first commit

---
+++ src/main/java/com/abs/AbsPrac1Application.java
@@ -0,0 +1,13 @@
+package com.abs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsPrac1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbsPrac1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/abs/AbsPrac1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbsPrac1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
