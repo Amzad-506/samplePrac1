ddb288b46284b9db730fa7de1ae6ee15eb8523c6
Amzad-506 <amzadbasha5066@gmail.com>
2024-10-02 23:47:05 +0530

    msg with count3

--- src/main/java/com/abs/controllers/ControlOne.java
+++ src/main/java/com/abs/controllers/ControlOne.java
@@ -8,9 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ControlOne {
 
+	int count =0 ;
 	@GetMapping("/welcome")
 	public ResponseEntity<String> welCome(){
-		int count =0 ;
+		
 		count++;
 		return new ResponseEntity<String>("Hello Azam"+count ,HttpStatus.OK);
 	}
